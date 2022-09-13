2aa5322d6b26176598e10a64ca8b8c07dccc559e
Reykim <mrayhanhakim2@gmail.com>
2022-09-14 00:23:58 +0700

    add name properties to routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,14 +2,16 @@ import { RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
   {
+    name: 'Home',
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
   },
   {
+    name: 'Character',
     path: '/characters',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/CharacterPage.vue') }]
+    children: [{ name: 'Character List', path: '', component: () => import('pages/CharacterPage.vue') }]
   },
 
   // Always leave this as last one,
