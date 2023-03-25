d995c69e6a45ec6fd6086e4969620448157ae75b
reykim7854 <mrayhanhakim2@gmail.com>
2023-03-25 11:58:33 +0700

    make student image card its own component

---
+++ src/components/Student/TableStudentCardImage.vue
@@ -0,0 +1,90 @@
+<style lang="sass" scoped>
+.student-card-size
+  min-width: 150px
+  max-width: 150px
+
+.img-caption
+  padding: 8px
+  background-color: rgba(0, 0, 0, 0.7)
+</style>
+
+<template>
+  <q-card class="student-card-size column q-ma-sm">
+    <q-img
+      :src="generateThumbImages(studentImages[row.name]['avatar'])"
+      :alt="`${row.name}'s Avatar'`"
+      height="169px"
+      loading="lazy"
+    >
+      <template v-slot:default>
+        <div class="img-caption absolute-bottom text-center text-subtitle2">
+          <template v-if="sortBy === 'baseStar' || sortBy === 'name'">
+            <q-icon
+              v-for="n in row.baseStar"
+              :key="`star-${n}`"
+              name="star"
+              color="yellow"
+            />
+          </template>
+          <template v-else>{{ formatSortByValue(row[sortBy]) }}</template>
+        </div>
+      </template>
+      <template v-slot:loading>
+        <div class="text-subtitle1 text-white">Loading...</div>
+      </template>
+      <template v-slot:error>
+        <div class="absolute-full flex flex-center bg-negative text-white">
+          Cannot load image
+        </div>
+      </template>
+    </q-img>
+    <q-card-section
+      style="white-space: pre-wrap"
+      class="text-center col-grow column justify-center text-subtitle1"
+    >
+      {{ formatName(row.name) }}
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TableStudentCardImage',
+  props: {
+    studentImages: {
+      type: Object,
+      default: () => Object,
+      required: true
+    },
+    sortBy: {
+      type: String,
+      default: 'name',
+      required: true
+    },
+    row: {
+      type: Object,
+      required: true
+    }
+  },
+
+  setup(props) {
+    const formatName = (val: string) => val.replace(/\(/, '\n(')
+    const formatSortByValue = (val: string) =>
+      val.replace(/([a-z])([A-Z])/g, '$1 $2')
+
+    const generateThumbImages = (url: string) => {
+      const newUrl = url.replace(/bluearchivewiki/, 'bluearchivewiki/thumb')
+      return `${newUrl}/150px-${url.substring(url.lastIndexOf('/') + 1)}`
+    }
+
+    return {
+      props,
+      formatName,
+      formatSortByValue,
+      generateThumbImages
+    }
+  }
+})
+</script>
